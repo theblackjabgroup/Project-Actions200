e6d7037d61b18f7b90d2c8687e6c258ed3c78e90
GramNotes <gramnotes5@gmail.com>
2020-09-18 14:39:39 +0530

    first commit

---
+++ test/example.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+describe('Simple Tests', () => {
+ it('should return 2', () => {
+        assert.equal(1 + 1, 2);
+    });
+ it('should return 9', () => {
+        assert.equal(3 * 3, 9);
+    });
+ it('should return 1', () => {
+        assert.equal(6 / 6, 1);
+    });
+});
